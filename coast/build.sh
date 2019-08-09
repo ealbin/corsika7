d9b433ab1659cb712205ce46fa9623ed652e2642
Eric Albin <Eric.K.Albin@gmail.com>
2019-08-08 22:34:21 -0700

    wrote build.sh

---
+++ coast/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+COAST_VERSION='v4r5'
+
+if [ -z $ROOTSYS ]
+then
+    echo '$ROOTSYS not defined.'
+    exit 1
+fi
+
+ROOT_VERSION=$(root -q | grep 'Version' | awk '{print $3}' | awk -F '.' '{print $1}') || exit 1
+if (( ROOT_VERSION > 5 ))
+then
+    echo 'Only ROOT versions <= 5 are supported'
+    exit 1
+fi    
+
+export COAST_DIR="$(pwd)/$COAST_VERSION"
+[ ! -d $COAST_DIR ] && mkdir $COAST_DIR
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COAST_DIR/lib"
+export COAST_USER_LIB="$(pwd)/src/coast-interfaces-v4r1p3/CorsikaRead"
+
+cd "./src/coast-$COAST_VERSION"
+./configure --with-root=yes && \
+make install && \
+printf "\n\nFinished successfully!\n"
+
