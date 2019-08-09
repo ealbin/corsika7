980d67f8f9f351bf13a4063f75b855749cd88746
Eric Albin <Eric.K.Albin@gmail.com>
2019-08-09 09:52:04 -0700

    updated readme

--- coast-init.sh
+++ coast-init.sh
@@ -4,5 +4,6 @@ COAST_VERSION='v4r5'
 
 export COAST_DIR="$(pwd)/coast/$COAST_VERSION"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COAST_DIR/lib"
-export COAST_USER_LIB="$(pwd)/coast/src/coast-interfaces-v4r1p3/Histogram"
+# optional features:
+#export COAST_USER_LIB="$(pwd)/coast/src/coast-interfaces-v4r1p3/Histogram"
 
