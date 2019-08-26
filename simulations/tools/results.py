5508e17929f45604cf1b44eebcf0d401565217da
Eric Albin <Eric.K.Albin@gmail.com>
2019-08-25 18:49:43 -0700

    tools work! ready for data

--- simulations/tools/results.py
+++ simulations/tools/results.py
@@ -117,7 +117,8 @@ class Results:
                 catagory = nuclei.ID[str(pid)[-2:]]['catagory']
             else:
                 print(' [ERROR] Unknown particle ID: {}'.format(pid))
-            
+                continue
+
             r = np.sqrt(x*x + y*y)
             self.density[catagory].Fill(r)
 
@@ -218,18 +219,20 @@ class Results:
                 catagory = nuclei.ID[str(pid)[-2:]]['catagory']
             else:
                 print(' [ERROR] Unknown particle ID: {}'.format(pid))
-            
+                continue
+
             # "x" is West-to-East (i.e. -y)
             # "y" is South-to-North (i.e. x)
             self.slice[catagory].Fill(-y, x)
 
         try:
-            self.slice_canvas
+            self.slice_canvases
         except:
             pass
         else:
-            del self.slice_canvas
+            del self.slice_canvases
 
+        R.gStyle.SetPalette(settings.Palette)
         name   = 'slice_canvas'
         width  = settings.Slice['width']
         height = settings.Slice['height']
@@ -260,6 +263,216 @@ class Results:
             legend.AddEntry(self.slice[_], settings.Symbols[_], '')
             legend.Draw()
             canvas.Update()
+
+    ##########################################################################
+
+    def plot_spectrum(self, obs_level=-1):
+        if (not self.all_good):
+            self.__print_std_err__()
+            return
+
+        maxlevel = self.run.run_ObservationLevel.size() - 1
+        if (obs_level < -1 or obs_level > maxlevel):
+            print(' [ERROR] Invalid observation level index')
+            self.observation_levels()
+            return
+
+        title = 'Composit of All Observation Levels'
+        if (obs_level != -1):
+            index = obs_level - 1
+            if (obs_level == 0):
+                index = 9
+            title = 'Observation Level = {:.2f} [km]'.format(
+                    self.run.run_ObservationLevel[index]/1e5)
+
+        for _ in settings.Catagories:
+            self.spectrum[_].SetTitle(title)
+            self.spectrum[_].Reset('ICEM')
+
+        for _ in range(self.sim.particle__):
+            
+            if (obs_level != -1):
+                if (self.sim.particle__ObservationLevel[_] != obs_level):
+                    continue
+
+            pid = self.sim.particle__ParticleID[_]
+            px  = self.sim.particle__Px[_] # GeV
+            py  = self.sim.particle__Py[_] # GeV
+            pz  = self.sim.particle__Pz[_] # GeV
+
+            if (str(pid) in particle.ID):
+                catagory = particle.ID[str(pid)]['catagory']
+                mass     = particle.ID[str(pid)]['mass'] # GeV
+            elif (str(pid)[-2:] in nuclei.ID):
+                catagory = nuclei.ID[str(pid)[-2:]]['catagory']
+                mass     = nuclei.ID[str(pid)[-2:]]['mass'] # GeV
+            else:
+                print(' [ERROR] Unknown particle ID: {}'.format(pid))
+                continue
+
+            energy = np.sqrt(px*px + py*py + pz*pz + mass*mass) * 1e9 # eV
+            self.spectrum[catagory].Fill(energy)
+
+        xaxis = self.spectrum[settings.Catagories[0]].GetXaxis()
+        for _ in range(xaxis.GetNbins()):
+            i     = _ + 1
+            width = xaxis.GetBinWidth(i)
+
+            for cat in settings.Catagories:
+                raw = self.spectrum[cat].GetBinContent(_)
+                self.spectrum[cat].SetBinContent(_, raw / width)
+
+                raw = self.spectrum[cat].GetBinError(_)
+                self.spectrum[cat].SetBinError(_, raw / width)
+
+        try:
+            self.spectrum_canvas
+        except:
+            pass
+        else:
+            del self.spectrum_canvas
+
+        name   = 'spectrum_canvas'
+        width  = settings.Density['width']
+        height = settings.Density['height']
+        left   = settings.Margins['left']
+        right  = settings.Margins['right']
+        bottom = settings.Margins['bottom']
+        top    = settings.Margins['top']
+        self.spectrum_canvas = R.TCanvas(name, title, width, height)
+        self.spectrum_canvas.SetFillStyle(4000)
+        self.spectrum_canvas.SetMargin(left, right, bottom, top)
+        self.spectrum_canvas.SetLogx()
+        self.spectrum_canvas.SetLogy()
+
+        for _ in settings.Catagories:
+            self.spectrum[_].SetMinimum(np.power(10., settings.Spectrum['ymin']))
+            self.spectrum[_].SetMaximum(np.power(10., settings.Spectrum['ymax']))
+            self.spectrum[_].Draw('hist pe same')
+
+        legend = R.TLegend(.6, .89, .94, .7)
+        legend.SetFillStyle(4000)
+        legend.SetBorderSize(0)
+        legend.SetNColumns(2)
+        legend.AddEntry(self.density['photon'],   '#gamma',      'pe')
+        legend.AddEntry(self.density['proton'],   'p, #bar{p}',  'pe')
+        legend.AddEntry(self.density['electron'], 'e^{#pm}',     'pe')
+        legend.AddEntry(self.density['neutron'],  'n, #bar{n}',  'pe')
+        legend.AddEntry(self.density['muon'],     '#mu^{#pm}',   'pe')
+        legend.AddEntry(self.density['ocharged'], 'other^{#pm}', 'pe')
+        legend.AddEntry(self.density['nuclei'],   'nuclei',      'pe')
+        legend.AddEntry(self.density['oneutral'], 'other^{0}',   'pe')
+        legend.Draw()
+        self.spectrum_canvas.Update()
+
+    ##########################################################################
+
+    def plot_content(self, obs_level=-1):
+        if (not self.all_good):
+            self.__print_std_err__()
+            return
+
+        maxlevel = self.run.run_ObservationLevel.size() - 1
+        if (obs_level < -1 or obs_level > maxlevel):
+            print(' [ERROR] Invalid observation level index')
+            self.observation_levels()
+            return
+
+        title = 'Composit of All Observation Levels'
+        if (obs_level != -1):
+            index = obs_level - 1
+            if (obs_level == 0):
+                index = 9
+            title = 'Observation Level = {:.2f} [km]'.format(
+                    self.run.run_ObservationLevel[index]/1e5)
+
+        self.content.SetTitle(title)
+        self.content.Reset('ICEM')
+
+        for _ in range(self.sim.particle__):
+            
+            if (obs_level != -1):
+                if (self.sim.particle__ObservationLevel[_] != obs_level):
+                    continue
+
+            pid = self.sim.particle__ParticleID[_]
+
+            if (str(pid) in particle.ID):
+                catagory = particle.ID[str(pid)]['catagory']
+            elif (str(pid)[-2:] in nuclei.ID):
+                catagory = nuclei.ID[str(pid)[-2:]]['catagory']
+            else:
+                print(' [ERROR] Unknown particle ID: {}'.format(pid))
+                continue
+
+            center = self.content.GetBinCenter(settings.Content[catagory])
+            self.content.Fill(center)
+
+        try:
+            self.content_canvas
+        except:
+            pass
+        else:
+            del self.content_canvas
+
+        name   = 'content_canvas'
+        width  = settings.Density['width']
+        height = settings.Density['height']
+        left   = settings.Margins['left']
+        right  = settings.Margins['right']
+        bottom = settings.Margins['bottom']
+        top    = settings.Margins['top']
+        self.content_canvas = R.TCanvas(name, title, width, height)
+        self.content_canvas.SetFillStyle(4000)
+        self.content_canvas.SetMargin(left, right, bottom, top)
+        self.content_canvas.SetLogy()
+
+        self.content.SetMinimum(np.power(10., settings.Content['ymin']))
+        self.content.SetMaximum(np.power(10., settings.Content['ymax']))
+        self.content.Draw('hist pe same')
+        
+        self.content_canvas.Update()
+
+    ##########################################################################
+
+    def plot_impact(self):
+        if (not self.all_good):
+            self.__print_std_err__()
+            return
+
+        title = settings.Impact['title'] 
+        self.impact.SetTitle(title)
+        self.impact.Reset('ICEM')
+
+        x = self.sim.shower_FirstTarget
+        y = self.sim.shower_FirstHeight / 1e5 # [km]
+        self.impact.Fill(x, y)
+
+        try:
+            self.impact_canvas
+        except:
+            pass
+        else:
+            del self.impact_canvas
+
+        R.gStyle.SetPalette(settings.Palette)
+        name   = 'impact_canvas'
+        width  = settings.Impact['width']
+        height = settings.Impact['height']
+        left   = settings.Margins['left']
+        right  = .17 #settings.Margins['right']
+        bottom = settings.Margins['bottom']
+        top    = settings.Margins['top']
+        self.impact_canvas = R.TCanvas(name, title, width, height)
+        self.impact_canvas.SetFillStyle(4000)
+        self.impact_canvas.SetMargin(left, right, bottom, top)
+        self.impact_canvas.SetLogz()
+
+        self.impact.SetMinimum(np.power(10., settings.Impact['zmin']))
+        self.impact.SetMaximum(np.power(10., settings.Impact['zmax']))
+        self.impact.Draw('colz')
+        
+        self.impact_canvas.Update()
 
     ##########################################################################
 
@@ -344,6 +557,8 @@ class Results:
         for _ in settings.Catagories:
             self.spectrum[_] = R.TH1D(name + _, title, xbins, xmin, xmax)
             self.spectrum[_].SetStats(stats)
+            self.spectrum[_].SetMarkerColor(settings.Colors[_])
+            self.spectrum[_].SetMarkerStyle(R.kFullCircle)
             self.spectrum[_].SetLineColor(settings.Colors[_])
             self.spectrum[_].GetXaxis().SetTitleOffset(settings.TitleOffset['x'])
             self.spectrum[_].GetYaxis().SetTitleOffset(settings.TitleOffset['y'])
@@ -360,34 +575,43 @@ class Results:
         xbins  = settings.Content['xbins']
         
         self.content = R.TH1D(name, title, xbins, 1, xbins+1)
+        self.content.SetStats(stats)
+        self.content.SetMarkerColor(R.kBlack)
+        self.content.SetMarkerStyle(R.kFullCircle)
+        self.content.SetLineColor(R.kBlack)
         self.content.GetXaxis().SetTitleOffset(settings.TitleOffset['x'])
         self.content.GetYaxis().SetTitleOffset(settings.TitleOffset['y'])
-        #bin_labels = settings.Content['bin_labels']
-        #for _ in range(1, xbins+1):
-        #   self.content.GetXaxis().SetBinLabel(_, bin_labels[_])
+        
+        axis = self.content.GetXaxis()
+        for _ in settings.Catagories:
+            axis.SetBinLabel(settings.Content[_], settings.Symbols[_])
 
 
         # First Impact
         #---------------------------------------------------------------------
         name   = settings.Impact['name']
         title  = settings.Impact['title']
+        xtitle = settings.Impact['xtitle']
         ytitle = settings.Impact['ytitle']
-        title  = '{};;{}'.format(title, ytitle)
+        title  = '{};{};{}'.format(title, xtitle, ytitle)
         stats  = settings.Impact['stats']
         xbins  = settings.Impact['xbins']
         ybins  = settings.Impact['ybins']
-        xmin   = settings.Impact['xmin']
         ymin   = settings.Impact['ymin']
-        xmax   = settings.Impact['xmax']
         ymax   = settings.Impact['ymax']
 
-        self.impact = R.TH2D(name, title, xbins, xmin, xmax, ybins, ymin, ymax)
+        self.impact = R.TH2D(name, title, xbins, 0, xbins, ybins, ymin, ymax)
+        self.impact.SetStats(stats)
+        self.impact.SetMarkerColor(R.kBlack)
+        self.impact.SetMarkerStyle(R.kFullCircle)
+        self.impact.SetLineColor(R.kBlack)
         self.impact.GetXaxis().SetTitleOffset(settings.TitleOffset['x'])
         self.impact.GetYaxis().SetTitleOffset(settings.TitleOffset['y'])
         self.__log_binning__(self.impact.GetYaxis())
-        #bin_labels = settings.Impact['bin_labels']
-        #for _ in range(1, xbins+1):
-        #   self.impact.GetXaxis().SetBinLabel(_, bin_labels[_])
+        
+        axis = self.impact.GetXaxis()
+        for _ in ['Random', 'Nitrogen', 'Oxygen', 'Argon']:
+            axis.SetBinLabel(settings.Impact[_] + 1, _)
 
     ##########################################################################
 
