c329c672852e19bfa790ebc62ccfb62901a23612
Eric Albin <Eric.K.Albin@gmail.com>
2019-08-28 13:18:10 -0700

    oops removed corsika76900 from the repository, added it back

---
+++ corsika-76900/include/config.h
@@ -0,0 +1,582 @@
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if BAACK package can be compiled on the system */
+/* #undef HAVE_BAACK */
+
+/* Define to 1 if BERNLOHR can be compiled on the system */
+/* #undef HAVE_BERNLOHR */
+
+/* Define to 1 if CERNLIB is available on the system */
+/* #undef HAVE_CERNLIB */
+
+/* Define to 1 if external COAST user library is available on the system */
+/* #undef HAVE_COASTUSERLIB */
+
+/* Define to 1 if CONEX is available on the system */
+/* #undef HAVE_CONEX */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if FLUKA is available on the system */
+/* #undef HAVE_FLUKA */
+
+/* Define to 1 if HERWIG is available on the system */
+/* #undef HAVE_HERWIG */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if Mpirunner is available on the system */
+/* #undef HAVE_MPIRUNNER_LIB */
+
+/* Define to 1 if PYTHIA is available on the system */
+/* #undef HAVE_PYTHIA */
+
+/* Define to 1 if ROOT is available on the system */
+/* #undef HAVE_ROOT */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the file `/etc/issue'. */
+/* #undef HAVE__ETC_ISSUE */
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "corsika"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "corsika"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "corsika 76900"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "corsika"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "76900"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "76900"
+
+/* produce analysis histograms */
+/* #undef __ANAHIST__ */
+
+/* external atmosphere */
+/* #undef __ATMEXT__ */
+
+/* cerenkov longitudinal profile */
+/* #undef __AUGCERLONG__ */
+
+/* augerhist */
+/* #undef __AUGERHIST__ */
+
+/* augerhit */
+/* #undef __AUGERHIT__ */
+
+/* augerinfo */
+/* #undef __AUGERINFO__ */
+
+/* BAACK modules with C++0x option */
+/* #undef __BAACKC0X__ */
+
+/* BAACK modules with C++11 option */
+/* #undef __BAACKC11__ */
+
+/* BAACK modules with C++14 option */
+/* #undef __BAACKC14__ */
+
+/* BAACK modules with C++1z option */
+/* #undef __BAACKC1Z__ */
+
+/* RECL parameter */
+#define __BYTERECL__ 1
+
+/* coconut configration cache for option ANAHIST */
+/* #undef __CACHE_ANAHIST__ */
+
+/* coconut configration cache for option ANNITEST */
+/* #undef __CACHE_ANNITEST__ */
+
+/* coconut configration cache for option ATMEXT */
+/* #undef __CACHE_ATMEXT__ */
+
+/* coconut configration cache for option AUGCERLONG */
+/* #undef __CACHE_AUGCERLONG__ */
+
+/* coconut configration cache for option AUGERHIST */
+/* #undef __CACHE_AUGERHIST__ */
+
+/* coconut configration cache for option AUGERHIT */
+/* #undef __CACHE_AUGERHIT__ */
+
+/* coconut configration cache for option AUGERINFO */
+/* #undef __CACHE_AUGERINFO__ */
+
+/* coconut configration cache for option BAACKC0X */
+/* #undef __CACHE_BAACKC0X__ */
+
+/* coconut configration cache for option BAACKC11 */
+/* #undef __CACHE_BAACKC11__ */
+
+/* coconut configration cache for option BAACKC14 */
+/* #undef __CACHE_BAACKC14__ */
+
+/* coconut configration cache for option BAACKC1Z */
+/* #undef __CACHE_BAACKC1Z__ */
+
+/* coconut configration cache for option CEFFIC */
+/* #undef __CACHE_CEFFIC__ */
+
+/* coconut configration cache for option CERENKOV */
+/* #undef __CACHE_CERENKOV__ */
+
+/* coconut configration cache for option CERWLEN */
+/* #undef __CACHE_CERWLEN__ */
+
+/* coconut configration cache for option CHARM */
+/* #undef __CACHE_CHARM__ */
+
+/* coconut configration cache for option COASTUSERLIB */
+/* #undef __CACHE_COASTUSERLIB__ */
+
+/* coconut configration cache for option COAST_LIB */
+/* #undef __CACHE_COAST_LIB__ */
+
+/* coconut configration cache for option COMPACT */
+/* #undef __CACHE_COMPACT__ */
+
+/* coconut configration cache for option CONEX */
+/* #undef __CACHE_CONEX__ */
+
+/* coconut configration cache for option COREAS */
+/* #undef __CACHE_COREAS__ */
+
+/* coconut configration cache for option CURVED */
+/* #undef __CACHE_CURVED__ */
+
+/* coconut configration cache for option DEV */
+/* #undef __CACHE_DEV__ */
+
+/* coconut configration cache for option DIST */
+/* #undef __CACHE_DIST__ */
+
+/* coconut configration cache for option DPMJET */
+/* #undef __CACHE_DPMJET__ */
+
+/* coconut configration cache for option DYNCUSTOM */
+/* #undef __CACHE_DYNCUSTOM__ */
+
+/* coconut configration cache for option DYNDEFAULT */
+/* #undef __CACHE_DYNDEFAULT__ */
+
+/* coconut configration cache for option DYNICECUBE */
+/* #undef __CACHE_DYNICECUBE__ */
+
+/* coconut configration cache for option DYNOUTPUT */
+/* #undef __CACHE_DYNOUTPUT__ */
+
+/* coconut configration cache for option DYNSTACK */
+/* #undef __CACHE_DYNSTACK__ */
+
+/* coconut configration cache for option EFIELD */
+/* #undef __CACHE_EFIELD__ */
+
+/* coconut configration cache for option EHISTORY */
+/* #undef __CACHE_EHISTORY__ */
+
+/* coconut configration cache for option EPOS */
+/* #undef __CACHE_EPOS__ */
+
+/* coconut configration cache for option FLUKA */
+/* #undef __CACHE_FLUKA__ */
+
+/* coconut configration cache for option GHEISHA */
+#define __CACHE_GHEISHA__ /**/
+
+/* coconut configration cache for option HERWIG */
+/* #undef __CACHE_HERWIG__ */
+
+/* coconut configration cache for option HORIZONTAL */
+#define __CACHE_HORIZONTAL__ /**/
+
+/* coconut configration cache for option IACTEXT */
+/* #undef __CACHE_IACTEXT__ */
+
+/* coconut configration cache for option IACT */
+/* #undef __CACHE_IACT__ */
+
+/* coconut configration cache for option ICECUBE1 */
+/* #undef __CACHE_ICECUBE1__ */
+
+/* coconut configration cache for option ICECUBE2 */
+/* #undef __CACHE_ICECUBE2__ */
+
+/* coconut configration cache for option INCLINED */
+/* #undef __CACHE_INCLINED__ */
+
+/* coconut configration cache for option INTCLONG */
+/* #undef __CACHE_INTCLONG__ */
+
+/* coconut configration cache for option INTTEST */
+/* #undef __CACHE_INTTEST__ */
+
+/* coconut configration cache for option KEEPSOURCE */
+/* #undef __CACHE_KEEPSOURCE__ */
+
+/* coconut configration cache for option LPM */
+/* #undef __CACHE_LPM__ */
+
+/* coconut configration cache for option M32 */
+/* #undef __CACHE_M32__ */
+
+/* coconut configration cache for option MACHINEINDEPENDENT */
+/* #undef __CACHE_MACHINEINDEPENDENT__ */
+
+/* coconut configration cache for option MPIRUNNER_LIB */
+/* #undef __CACHE_MPIRUNNER_LIB__ */
+
+/* coconut configration cache for option MULTITHIN */
+/* #undef __CACHE_MULTITHIN__ */
+
+/* coconut configration cache for option MUONHIST */
+/* #undef __CACHE_MUONHIST__ */
+
+/* coconut configration cache for option MUPROD */
+/* #undef __CACHE_MUPROD__ */
+
+/* coconut configration cache for option NEUTRINO */
+/* #undef __CACHE_NEUTRINO__ */
+
+/* coconut configration cache for option NEXUS */
+/* #undef __CACHE_NEXUS__ */
+
+/* coconut configration cache for option NOCLONG */
+/* #undef __CACHE_NOCLONG__ */
+
+/* coconut configration cache for option NOCOMPILE */
+/* #undef __CACHE_NOCOMPILE__ */
+
+/* coconut configration cache for option OUT32 */
+/* #undef __CACHE_OUT32__ */
+
+/* coconut configration cache for option PARALLELIB */
+/* #undef __CACHE_PARALLELIB__ */
+
+/* coconut configration cache for option PARALLEL */
+/* #undef __CACHE_PARALLEL__ */
+
+/* coconut configration cache for option PLOTSH2 */
+/* #undef __CACHE_PLOTSH2__ */
+
+/* coconut configration cache for option PLOTSH */
+/* #undef __CACHE_PLOTSH__ */
+
+/* coconut configration cache for option PRESHOWER */
+/* #undef __CACHE_PRESHOWER__ */
+
+/* coconut configration cache for option QGSJET01 */
+#define __CACHE_QGSJET01__ /**/
+
+/* coconut configration cache for option QGSJETII */
+/* #undef __CACHE_QGSJETII__ */
+
+/* coconut configration cache for option REMOTECONTROL */
+/* #undef __CACHE_REMOTECONTROL__ */
+
+/* coconut configration cache for option RIGIDITY */
+/* #undef __CACHE_RIGIDITY__ */
+
+/* coconut configration cache for option ROOTOUT */
+/* #undef __CACHE_ROOTOUT__ */
+
+/* coconut configration cache for option SIBYLL */
+/* #undef __CACHE_SIBYLL__ */
+
+/* coconut configration cache for option SLANT */
+/* #undef __CACHE_SLANT__ */
+
+/* coconut configration cache for option STACKIN */
+/* #undef __CACHE_STACKIN__ */
+
+/* coconut configration cache for option TAULEP */
+/* #undef __CACHE_TAULEP__ */
+
+/* coconut configration cache for option THIN */
+/* #undef __CACHE_THIN__ */
+
+/* coconut configration cache for option TRAJECT */
+/* #undef __CACHE_TRAJECT__ */
+
+/* coconut configration cache for option UPWARD */
+/* #undef __CACHE_UPWARD__ */
+
+/* coconut configration cache for option URQMD */
+/* #undef __CACHE_URQMD__ */
+
+/* coconut configration cache for option VENUS */
+/* #undef __CACHE_VENUS__ */
+
+/* coconut configration cache for option VIEWCONE */
+/* #undef __CACHE_VIEWCONE__ */
+
+/* coconut configration cache for option VOLUMECORR */
+/* #undef __CACHE_VOLUMECORR__ */
+
+/* coconut configration cache for option VOLUMEDET */
+/* #undef __CACHE_VOLUMEDET__ */
+
+/* simulate telescope detector */
+/* #undef __CEFFIC__ */
+
+/* for Cherenkov grid detector */
+/* #undef __CERENKOV__ */
+
+/* Cherenkov wavelength dependent */
+/* #undef __CERWLEN__ */
+
+/* pythia charm production */
+/* #undef __CHARM__ */
+
+/* Inclined observation level */
+/* #undef __COASTUSERLIB__ */
+
+/* ENABLE COAST INTERFACE */
+/* #undef __COAST__ */
+
+/* compact output */
+/* #undef __COMPACT__ */
+
+/* CONEX cascade equation (CONEX) */
+/* #undef __CONEX__ */
+
+/* curved atmosphere */
+/* #undef __CURVED__ */
+
+/* to compile without optimisation and system flags */
+/* #undef __DEV__ */
+
+/* to make dist */
+/* #undef __DIST__ */
+
+/* include DPMJET 2.55 program */
+/* #undef __DPMJET__ */
+
+/* user defined intermediate particle stack */
+/* #undef __DYNCUSTOM__ */
+
+/* emulation of normal stack */
+/* #undef __DYNDEFAULT__ */
+
+/* reimplementation of the ICECUBE1 module */
+/* #undef __DYNICECUBE__ */
+
+/* stores every saved particle to file */
+/* #undef __DYNOUTPUT__ */
+
+/* DYNamic particle STACK */
+/* #undef __DYNSTACK__ */
+
+/* electrical field */
+/* #undef __EFIELD__ */
+
+/* particle history */
+/* #undef __EHISTORY__ */
+
+/* include EPOS program */
+/* #undef __EPOS__ */
+
+/* include FLUKA 2011 program */
+/* #undef __FLUKA__ */
+
+/* In case of GFORTRAN compiler (GCC > 4) */
+#define __GFORTRAN__ 1
+
+/* include GHEISHA program */
+#define __GHEISHAD__ 1
+
+/* for external IACT Cherenkov file */
+/* #undef __IACTEXT__ */
+
+/* for Cherenkov telescope detector */
+/* #undef __IACT__ */
+
+/* ibmrisc routine */
+/* #undef __IBMRISC__ */
+
+/* ICECUBE FiFo */
+/* #undef __ICECUBE1__ */
+
+/* ICECUBE gzip/pipe output */
+/* #undef __ICECUBE2__ */
+
+/* In case of IFORT compiler */
+/* #undef __IFORT__ */
+
+/* new Cherenkov counting */
+/* #undef __INTCLONG__ */
+
+/* interaction test */
+/* #undef __INTTEST__ */
+
+/* FLUKA 2011 for Linux */
+/* #undef __LINUX__ */
+
+/* LPM effect */
+/* #undef __LPM__ */
+
+/* explicitly use the -m32 flag */
+/* #undef __M32__ */
+
+/* In case of Apple computer */
+/* #undef __MAC__ */
+
+/* for multiple thinning */
+/* #undef __MULTITHIN__ */
+
+/* muonhist */
+/* #undef __MUONHIST__ */
+
+/* MUPROD */
+/* #undef __MUPROD__ */
+
+/* neutrino primaries */
+/* #undef __NEUTRINO__ */
+
+/* include neXus 3.97 program */
+/* #undef __NEXUS__ */
+
+/* no cherenkov longitudinla profile */
+/* #undef __NOCLONG__ */
+
+/* do not compile binaries, just extract CORSIKA compilefile */
+/* #undef __NOCOMPILE__ */
+
+/* herwig neutrino production (NUPRIM) */
+/* #undef __NUPRIM__ */
+
+/* official version marker */
+#define __OFFIC__ 1
+
+/* old date and time routine for LINUX beta */
+/* #undef __OLDDATE2__ */
+
+/* old date and time routine */
+/* #undef __OLDDATE__ */
+
+/* explicitly use the -frecord-marker=4 flag */
+/* #undef __OUT32__ */
+
+/* parallel computation with MPI */
+/* #undef __PARALLELIB__ */
+
+/* parallel computation */
+/* #undef __PARALLEL__ */
+
+/* plot shower C */
+/* #undef __PLOTSH2__ */
+
+/* plot shower */
+/* #undef __PLOTSH__ */
+
+/* photon preshower */
+/* #undef __PRESHOWER__ */
+
+/* include QGSJET II program */
+/* #undef __QGSII__ */
+
+/* include QGSJET 01C program */
+#define __QGSJETOLD__ 1
+
+/* include QGSJET01/QGSJET II program */
+#define __QGSJET__ 1
+
+/* Remote acces to corsika */
+/* #undef __REMOTECONTROL__ */
+
+/* rigidity */
+/* #undef __RIGIDITY__ */
+
+/* to keep the CORSIKA compilefile */
+/* #undef __SAVEDCORS__ */
+
+/* include Sybill 2.3 program */
+/* #undef __SIBYLL__ */
+
+/* slant profile */
+/* #undef __SLANT__ */
+
+/* stack input */
+/* #undef __STACKIN__ */
+
+/* pythia tau lepton production */
+/* #undef __TAULEP__ */
+
+/* annitest */
+/* #undef __TEST__ */
+
+/* for thinning */
+/* #undef __THIN__ */
+
+/* timerc routine */
+#define __TIMERC__ 1
+
+/* trajectory */
+/* #undef __TRAJECT__ */
+
+/* In case of UNIX computer */
+#define __UNIX__ 1
+
+/* upward particles */
+/* #undef __UPWARD__ */
+
+/* include UrQMD 1.3 program */
+/* #undef __URQMD__ */
+
+/* include VENUS 4.12 program */
+/* #undef __VENUS__ */
+
+/* viewcone */
+/* #undef __VIEWCONE__ */
+
+/* detector geometry (vertical string) */
+/* #undef __VOLUMECORR__ */
+
+/* detector geometry (volume) */
+/* #undef __VOLUMEDET__ */
