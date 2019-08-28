8df2a765289bd2fb6e5f53d9b4909448111fce69
Eric Albin <Eric.K.Albin@gmail.com>
2019-08-27 20:09:43 -0700

    cleaned up template

---
+++ simulations/template/parallel.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+# Example parallel job script for UCI greenplanet cluster
+
+#SBATCH --job-name=my_job               # <-- Edit this
+#SBATCH -p a_partition_i_can_use        # <-- Edit this (e.g. nes2.8)
+#SBATCH -c 1                            # cpus-per-task, 1 is correct
+#SBATCH -n 123                          # <-- Edit this (how many cores do you want)
+#SBATCH -t 2-8:00                       # <-- Edit this (days-hours:minutes)
+#SBATCH -o /path/slurm_output.txt       # <-- Edit this
+
+here=$(pwd)
+infile=$here/input.txt                  # <-- Edit this if you need to (!! don't forget to uncomment PARALLEL in input.txt !!)
+outfile=$here/parallel_output.txt                       # <-- Edit this if you want
+executable="corsika76900Linux_QGSJET_gheisha_parallel"  # <-- !!! Definitely Edit This !!!
+
+cd /your/path/to/corsika-76900/run                      # <-- Edit this
+
+srun ./$executable < $infile > $outfile 
+printf "\nFinished!\n"
+
+# submit this to slurm with:
+# $ sbatch parallel.sh
+
+# Optional, when you're done, if you've compiled corsika2root (see ../../coast/README)
+# Do this from the command line:
+# $ ../../coast/v4r5/bin/coast2root DATxxxxxxxx  <-- whatever your DAT file is
+# (note: be sure to have sourced your/path/to/root/thisroot.sh version < 6 before-hand)
+#
+# Check out some PyROOT tools in ../tools
---
+++ simulations/template/single.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Example single-core job script
+
+here=$(pwd)
+infile=$here/input.txt                          # <-- Edit this if you need to
+outfile=$here/single_output.txt                 # <-- Edit this if you want
+executable="corsika76900Linux_QGSJET_gheisha"   # <-- !!! Definitely Edit This !!!
+
+cd /your/path/to/corsika-76900/run              # <-- Edit this
+
+/usr/bin/time -v -a -o $out_file ./$executable < $in_file > $out_file 
+
+printf "\nFinished!\n"
+
+# run:
+# $ ./single.sh
+
+# Optional, when you're done, if you've compiled corsika2root (see ../../coast/README)
+# Do this from the command line:
+# $ ../../coast/v4r5/bin/coast2root DATxxxxxxx  <-- whatever your DAT file is
+# (note: be sure to have sourced your/path/to/root/thisroot.sh version < 6 before-hand)
+#
+# Check out some PyROOT tools in ../tools
