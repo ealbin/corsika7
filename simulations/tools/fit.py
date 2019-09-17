092b94cc78b98958d53faeb532ac64ff804543e7
Eric Albin <Eric.K.Albin@gmail.com>
2019-09-17 16:39:37 -0700

    fit work in progress

--- simulations/tools/fit.py
+++ simulations/tools/fit.py
@@ -2,6 +2,7 @@
 import os
 import numpy as np
 import pandas as pd
+from scipy.special import gamma
 import ROOT as R
 from . import plot
 
@@ -16,9 +17,178 @@ def E_func(E):
 def H_func(H):
     return H
 
+class NKG:
+    name     = 'NKG'
+    fitfunc  = '10^[0] * x^(-[1]) * (1. + x/[2])^(-[3])'
+    equation = '10^{a_{0}} x^{-a_{1}} (1 + x / a_{2})^{-a_{3}}'
 
-def quick_check(indir, channel='muon'):
-    df = pd.read_csv(os.path.join(indir, 'coeff_data_{}.txt'.format(channel)), delim_whitespace=True)
+    a0_val =  -5.
+    a0_low = -10.
+    a0_hi  =  10.
+
+    a1_val =   1.
+    a1_low =  -1.
+    a1_hi  =   5.
+
+    a2_val =   1.
+    a2_low =  -2.
+    a2_hi  =  20.
+
+    a3_val =   5.
+    a3_low =  -2.
+    a3_hi  = 100.
+
+
+class Albin:
+    name     = 'Albin'
+    fitfunc  = 'x^(-[0]) * exp(-[1] - [2] * x^[3])'
+    equation = 'x^{-a_{0}} exp(-a_{1} - a_{2} x^{a_{3}})' 
+
+    a0_val =   0.5
+    a0_low = -10.
+    a0_hi  =  20.
+
+    a1_val =   5.
+    a1_low = -30.
+    a1_hi  =  30.
+
+    a2_val =   5.
+    a2_low = -10.
+    a2_hi  =  35.
+
+    a3_val =  0.5
+    a3_low = -5.
+    a3_hi  =  5.
+
+__XMIN__ = 1e-6
+__XMAX__ = 1e3
+
+class Density:
+
+    def __init__(self):
+        pass
+
+    def plot_density(self, indir, mass_number, energy, altitude, channel='muon'):
+        nkg = pd.read_csv(os.path.join(indir, '{}_{}_model.txt'.format(NKG.name, channel)), delim_whitespace=True)
+        albin = pd.read_csv(os.path.join(indir, '{}_{}_model.txt'.format(Albin.name, channel)), delim_whitespace=True)
+
+        fnkg = R.TF1('NKG',   NKG.fitfunc,   __XMIN__, __XMAX__)
+        falb = R.TF1('Albin', Albin.fitfunc, __XMIN__, __XMAX__)
+
+        A = A_func(mass_number)
+        E = E_func(energy)
+        H = H_func(altitude)
+
+        X = np.asarray([1., A, E, H, A*A, A*E, A*H, E*E, E*H, H*H])
+        for i in range(4):
+            fnkg.SetParameter(i, np.dot(X, nkg.iloc[i,1:]))
+            falb.SetParameter(i, np.dot(X, albin.iloc[i,1:]))
+
+        self.canvas = plot.make1Dcanvas('canvas')
+        self.canvas.SetLogx()
+        self.canvas.SetLogy()
+
+        legend = plot.make1Dlegend()
+        fnkg.SetLineColor(R.kRed)
+        falb.SetLineColor(R.kBlue)
+        legend.AddEntry(fnkg, 'NKG', 'l')
+        legend.AddEntry(falb, 'Albin', 'l')
+
+        #fnkg.Draw()
+        #falb.Draw('same')
+        falb.Draw()
+        legend.Draw()
+        self.canvas.Update()
+
+
+def likelihood_ratio(infile, indir, model0=NKG, model1=Albin, channel='muon'):
+    model0_coeff = pd.read_csv(os.path.join(indir, '{}_{}_model.txt'.format(model0.name, channel)), delim_whitespace=True)
+    model1_coeff = pd.read_csv(os.path.join(indir, '{}_{}_model.txt'.format(model1.name, channel)), delim_whitespace=True)
+    
+    alt_km = [0., 100., 50., 20., 10., 5., 2., 1.4, 1.0, .5]
+    MassNumber = {'He':4, 'O':16, 'Fe':56}
+
+    thin      = '1E-6'
+    N         = '100'
+    primaries = ['He', 'O', 'Fe']
+    energies  = ['1e14', '1e15', '1e16', '1e17', '1e18', '1e19', '1e20', '1e21']
+    altitudes = ['3', '4', '5', '6', '7', '8', '9', '0']
+    models    = ['DPMJET', 'QGSJET', 'QGSII', 'SIBYLL', 'VENUS']
+    
+    f = R.TFile(infile)
+
+#    def poisson(l, k):
+#        return np.power(l, k) * np.exp(-l) / gamma(k + 1.)
+
+    with open('{}/{}_llratio.txt'.format(indir, channel), 'w') as outfile:
+        for primary in primaries:
+            for energy in energies:
+                for altitude in altitudes:
+
+                    model0_sum = 0.
+                    model1_sum = 0.
+
+                    name = 'THIN_{}_{}_density_{}_{}'.format(thin, N, channel, altitude)
+                    dpmjet = f.Get('{}/{}/{}/{}'.format(energy, 'DPMJET', primary, name))
+                    qgsjet = f.Get('{}/{}/{}/{}'.format(energy, 'QGSJET', primary, name))
+                    qgsii  = f.Get('{}/{}/{}/{}'.format(energy, 'QGSII',  primary, name))
+                    sibyll = f.Get('{}/{}/{}/{}'.format(energy, 'SIBYLL', primary, name))
+                    venus  = f.Get('{}/{}/{}/{}'.format(energy, 'VENUS',  primary, name))
+
+                    f0 = R.TF1('model0', model0.fitfunc, __XMIN__, __XMAX__)
+                    f1 = R.TF1('model1', model1.fitfunc, __XMIN__, __XMAX__)
+
+                    A = A_func(MassNumber[primary])
+                    E = E_func(float(energy))
+                    H = H_func(alt_km[int(altitude)])
+            
+                    X = np.asarray([1., A, E, H, A*A, A*E, A*H, E*E, E*H, H*H])
+                    for i in range(4):
+                        f0.SetParameter(i, np.dot(X, model0_coeff.iloc[i,1:]))
+                        f1.SetParameter(i, np.dot(X, model1_coeff.iloc[i,1:]))
+
+                    for h in [dpmjet, qgsjet, qgsii, sibyll, venus]:
+                        if (h != None):
+                            xaxis = h.GetXaxis()
+                            for bin in range(xaxis.GetNbins() + 1):
+                                center = xaxis.GetBinCenterLog(bin)
+                                binval = h.GetBinContent(bin)
+                                
+                                f0_val = f0.Eval(center)
+                                f1_val = f1.Eval(center)
+
+                                if (binval <= 0 or f0_val <= 0 or f1_val <= 0):
+                                    continue
+
+                                model0_sum += binval * np.log(f0_val) - f0_val - np.log(gamma(binval + 1.))
+                                model1_sum += binval * np.log(f1_val) - f1_val - np.log(gamma(binval + 1.))
+
+                                #model0_sum += np.log( poisson(f0.Eval(center), binval) )
+                                #model1_sum += np.log( poisson(f1.Eval(center), binval) )
+
+                    outfile.write('{:<20.10e}\n'.format(2*(model1_sum - model0_sum)))
+
+
+def density_coeff(indir, model=Albin, channel='muon'):
+    df = pd.read_csv(os.path.join(indir, '{}_coeff_data_{}.txt'.format(model.name, channel)), delim_whitespace=True)
+
+    df.insert(0, 'C', 1)
+    A = df.iloc[:,:10].to_numpy()
+    B = np.linalg.pinv(A)
+
+    with open(os.path.join(indir, '{}_{}_model.txt'.format(model.name, channel)), 'w') as outfile:
+        outfile.write( ('{:15s}'*11).format('a', 'C', 'A', 'E', 'H', 'AA', 'AE', 'AH', 'EE', 'EH', 'HH') + '\n')
+    
+        for i in range(4):
+            a = df.loc[:, 'a{}'.format(i)].to_numpy()
+            string = '{:<15d}'.format(i)
+            for coeff in np.dot(B, a):
+                string += '{:<15.6e}'.format(coeff)
+            outfile.write(string + '\n')
+
+
+def quick_check(indir, model=Albin, channel='muon'):
+    df = pd.read_csv(os.path.join(indir, '{}_coeff_data_{}.txt'.format(model.name, channel)), delim_whitespace=True)
 
     # no const
     A9 = df.iloc[:,:9].to_numpy()
@@ -65,10 +235,10 @@ def quick_check(indir, channel='muon'):
         print('a{}, no const std:  {:7.3f} vs const std:  {:7.3f}'.format(i, d9.std(), d10.std()))
         print()
 
-    plt.gcf().suptitle(channel) 
+    plt.gcf().suptitle('{} {}'.format(model.name, channel))
 
 
-def density_check(infile, indir, savedir='fit_check', batchmode=True):
+def density_check(infile, indir, model=Albin, savedir='fit_check', batchmode=True):
 
     if (batchmode == True):
         R.gROOT.SetBatch(R.kTRUE)
@@ -82,7 +252,7 @@ def density_check(infile, indir, savedir='fit_check', batchmode=True):
     thin      = '1E-6'
     N         = '100'
     primaries = ['He', 'O', 'Fe']
-    energies  = ['1e15', '1e16', '1e17', '1e18', '1e19', '1e20', '1e21']
+    energies  = ['1e14', '1e15', '1e16', '1e17', '1e18', '1e19', '1e20', '1e21']
     altitudes = ['3', '4', '5', '6', '7', '8', '9', '0']
     models    = ['DPMJET', 'QGSJET', 'QGSII', 'SIBYLL', 'VENUS']
     
@@ -92,7 +262,7 @@ def density_check(infile, indir, savedir='fit_check', batchmode=True):
     canvas.SetLogx()
     
     for channel in ['muon', 'photon']:
-        df = pd.read_csv(os.path.join(indir, 'coeff_data_{}.txt'.format(channel)), delim_whitespace=True)
+        df = pd.read_csv(os.path.join(indir, '{}_coeff_data_{}.txt'.format(model.name, channel)), delim_whitespace=True)
         df.insert(0, 'C', 1.)
         A = df.iloc[:,:10].to_numpy()
         B = np.linalg.pinv(A)
@@ -113,9 +283,16 @@ def density_check(infile, indir, savedir='fit_check', batchmode=True):
                     venus  = f.Get('{}/{}/{}/{}'.format(energy, 'VENUS',  primary, name))
 
                     hists = []
+                    wrong_lim = []
                     for h in [dpmjet, qgsjet, qgsii, sibyll, venus]:
                         if (h != None):
-                            hists.append(h)
+                            if (h.GetXaxis().GetXmin() > __XMIN__):
+                                wrong_lim.append(h)
+                            else:
+                                hists.append(h)
+
+                    if (len(wrong_lim) > len(hists)):
+                        hists = wrong_lim
 
                     if (len(hists) > 0):
                         hsum = hists[0]
@@ -137,8 +314,8 @@ def density_check(infile, indir, savedir='fit_check', batchmode=True):
                                 break
                         limit = xaxis.GetBinCenter(stop_bin)
 
-                        fitfunc = 'x^(-[0]) * exp(-[1] - [2] * x^[3])'
-                        f1 = R.TF1('fitfunc', fitfunc, 1e-3, limit)
+                        fitfunc = model.fitfunc
+                        f1 = R.TF1('fitfunc', fitfunc, __XMIN__, limit)
 
                         A = A_func(MassNumber[primary])
                         E = E_func(float(energy))
@@ -154,27 +331,28 @@ def density_check(infile, indir, savedir='fit_check', batchmode=True):
 
                         legend = plot.make1Dlegend()
                         legend.AddEntry(hsum, '{}: n={}, thin={}, model ave'.format(plot.__symbols__[channel], N, thin), 'p')
-                        legend.AddEntry(f1, 'x^{-a_{0}} exp(-a_{1} - a_{2} x^{a_{3}})', 'l')
+                        legend.AddEntry(f1, model.equation, 'l')
 
                         canvas.cd()
                         canvas.Clear()
+                        hsum.GetXaxis().SetRangeUser(__XMIN__, 1e3)
                         hsum.Draw('hist p')
                         f1.Draw('same')
                         legend.Draw()
                         canvas.Update()
-                        canvas.SaveAs('{}/check_{}_{}_{}_{}.png'.format(savedir, channel, primary, energy, altitude))
+                        canvas.SaveAs('{}/check_{}_{}_{}_{}_{}.png'.format(savedir, model.name, channel, primary, energy, altitude))
 
 
-def supervised_density(infile, savedir='fit_density'):
+def supervised_density(infile, savedir='fit_density', model=Albin):
 
     if (not os.path.isdir(savedir)):
         os.makedirs(savedir, exist_ok=True)
-    
+
     thin      = '1E-6'
     N         = '100'
     channels  = ['muon', 'photon']
     primaries = ['He', 'O', 'Fe']
-    energies  = ['1e15', '1e16', '1e17', '1e18', '1e19', '1e20', '1e21']
+    energies  = ['1e14', '1e15', '1e16', '1e17', '1e18', '1e19', '1e20', '1e21']
     altitudes = ['3', '4', '5', '6', '7', '8', '9', '0'] 
     models    = ['DPMJET', 'QGSJET', 'QGSII', 'SIBYLL', 'VENUS']
 
@@ -197,16 +375,19 @@ def supervised_density(infile, savedir='fit_density'):
         if (flush):
             file.flush()
 
-    muon   = open('{}/coeff_data_muon.txt'.format(savedir),   'w')
-    photon = open('{}/coeff_data_photon.txt'.format(savedir), 'w')
+    muon   = open('{}/{}_coeff_data_muon.txt'.format(savedir, model.name),   'w')
+    photon = open('{}/{}_coeff_data_photon.txt'.format(savedir, model.name), 'w')
 
     write_header(muon)
     write_header(photon)
 
+    total_fits = len(channels) * len(primaries) * len(energies) * len(altitudes)
+    fit_count  = 0
     for channel in channels:
         for primary in primaries:
             for energy in energies:
                 for altitude in altitudes:
+                    fit_count += 1
 
                     name = 'THIN_{}_{}_density_{}_{}'.format(thin, N, channel, altitude)
                     dpmjet = f.Get('{}/{}/{}/{}'.format(energy, 'DPMJET', primary, name))
@@ -216,9 +397,20 @@ def supervised_density(infile, savedir='fit_density'):
                     venus  = f.Get('{}/{}/{}/{}'.format(energy, 'VENUS',  primary, name))
 
                     hists = []
+                    wrong_min = []
                     for h in [dpmjet, qgsjet, qgsii, sibyll, venus]:
                         if (h != None):
-                            hists.append(h)
+                            if (h.GetXaxis().GetXmin() > __XMIN__):
+                                wrong_min.append(h)
+                            else:
+                                hists.append(h)
+
+                    if (len(wrong_min) > len(hists)):
+                        with open('{}/{}_info.txt'.format(savedir, model.name), 'w') as g:
+                            info = ' [INFO]: switching to wrong min for {}'.format(name)
+                            g.write(info + '\n')
+                            print(info)
+                        hists = wrong_min
 
                     if (len(hists) > 0):
                         hsum = hists[0]
@@ -240,25 +432,24 @@ def supervised_density(infile, savedir='fit_density'):
                                 break
                         limit = xaxis.GetBinCenter(stop_bin)
 
-                        fitfunc = '10^[0] * x^(-[1]) * (1. + x/[2])^(-[3])'
-                        #fitfunc = 'x^(-[0]) * exp(-[1] - [2] * x^[3])'
-                        f1 = R.TF1('fitfunc', fitfunc, 1e-3, limit)
+                        fitfunc = model.fitfunc
+                        f1 = R.TF1('fitfunc', fitfunc, __XMIN__, limit)
 
-                        a0_val = 1.    #.5
-                        a0_low = -50.  # -10.
-                        a0_hi  = 50.   # 20.
+                        a0_val = model.a0_val
+                        a0_low = model.a0_low
+                        a0_hi  = model.a0_hi
 
-                        a1_val = 1.    # 5.
-                        a1_low = -30.  # -30.
-                        a1_hi  = 30.   # 30.
+                        a1_val = model.a1_val
+                        a1_low = model.a1_low
+                        a1_hi  = model.a1_hi
 
-                        a2_val = 20.   # 5.
-                        a2_low = -100. # -10.
-                        a2_hi  = 200.  # 35.
+                        a2_val = model.a2_val
+                        a2_low = model.a2_low
+                        a2_hi  = model.a2_hi
 
-                        a3_val = 2.    # .5
-                        a3_low = -10.  # -5.
-                        a3_hi  = 20.   # 5.
+                        a3_val = model.a3_val
+                        a3_low = model.a3_low
+                        a3_hi  = model.a3_hi
 
                         f1.SetParameters(a0_val, a1_val, a2_val, a3_val)
                         f1.SetParLimits(0, a0_low, a0_hi)
@@ -272,7 +463,7 @@ def supervised_density(infile, savedir='fit_density'):
 
                         legend = plot.make1Dlegend()
                         legend.AddEntry(hsum, '{}: n={}, thin={}, model ave'.format(plot.__symbols__[channel], N, thin), 'p')
-                        legend.AddEntry(f1, 'x^{-a_{0}} exp(-a_{1} - a_{2} x^{a_{3}})', 'l')
+                        legend.AddEntry(f1, model.equation, 'l')
 
                         def rolldice(f1):
                             def rolldie(mu, low, hi):
@@ -290,11 +481,13 @@ def supervised_density(infile, savedir='fit_density'):
 
                         def query(f1):
                             while (True):
-                                action = input('(r)etry, (s)ave, (a)bandon: ')
+                                print()
+                                print('fitting {} of {} for: {}_{}_{}_{}_{}'.format(fit_count, total_fits, model.name, channel, primary, energy, altitude))
+                                action = input('>>> (r)etry, (s)ave, (a)bandon: ')
                                 if (action == 'r'):
                                     return 'r'
                                 if (action == 's'):
-                                    canvas.SaveAs('{}/{}_{}_{}_{}.png'.format(savedir, channel, primary, energy, altitude))
+                                    canvas.SaveAs('{}/{}_{}_{}_{}_{}.png'.format(savedir, model.name, channel, primary, energy, altitude))
                                     fit0 = f1.GetParameter(0)
                                     fit1 = f1.GetParameter(1)
                                     fit2 = f1.GetParameter(2)
@@ -311,13 +504,14 @@ def supervised_density(infile, savedir='fit_density'):
                         trys = 0
                         while (True):
                             fitstatus = int(hsum.Fit('fitfunc', 'RE Q'))
-                            if (fitstatus != 0 and trys < 10):
+                            if (fitstatus != 0 or f1.GetChisquare() > 1000 and trys < 100):
                                 trys += 1
                                 rolldice(f1)
                                 continue
 
                             canvas.cd()
                             canvas.Clear()
+                            hsum.GetXaxis().SetRangeUser(__XMIN__,1e3)
                             hsum.Draw('hist p')
                             f1.Draw('same')
                             legend.Draw()
